c4d8342acfa72ddff5778b75f615f4c043d3df89
EGRrqq <86194387+EGRrqq@users.noreply.github.com>
2023-03-02 21:08:18 +0300

    ex 13.16 | /api/authors

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const { connectToDatabase } = require('./utils/db')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
+const authorRouter = require('./controllers/authors')
 
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :req-body'))
@@ -20,6 +21,7 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :r
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
+app.use('/api/authors', authorRouter)
 
 const start = async () => {
   await connectToDatabase()
