7726912cce40a3dcf3507dda278e2821c4f836ab
EGRrqq <86194387+EGRrqq@users.noreply.github.com>
2023-03-04 00:39:40 +0300

    ex 13.22 / mark a blog in the reading list as read

--- controllers/readinglist.js
+++ controllers/readinglist.js
@@ -1,5 +1,11 @@
 const router = require('express').Router()
-const { ReadingList } = require('../models')
+const { ReadingList, User } = require('../models')
+const { tokenExtractor } = require('../utils/middleware')
+
+const readinglistFinder = async (req, res, next) => {
+  req.readinglistitem = await ReadingList.findByPk(req.params.id)
+  next()
+}
 
 router.post('/', async (req, res) => {
     const item = await ReadingList.create(req.body)
@@ -9,6 +15,23 @@ router.post('/', async (req, res) => {
 router.get('/', async (req, res) => {
     const readinglists = await ReadingList.findAll()
     res.json(readinglists)
+})
+
+router.put('/:id', tokenExtractor, readinglistFinder, async (req, res) => {
+    const user = await User.findByPk(req.decodedToken.id)
+    if (req.readinglistitem) {
+        
+      if (user.id === req.readinglistitem.userId) {
+        req.readinglistitem.read = req.body.read
+        await req.readinglistitem.save()
+        res.json(req.readinglistitem)
+      } else {
+        res.status(401).send("the user can mark blogs only in his own reading list as read")
+      }
+
+    } else {
+      res.status(404).end()
+    }
 })
 
 module.exports = router 
